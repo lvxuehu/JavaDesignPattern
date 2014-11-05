08c31e6b887d2f3a65371d9beaa060ac645ddd5a
JasonyLi <JasonyLi@163.com>
2014-11-05 22:34:30 +0800

    alter this factory class 2

--- src/com/pattern/lly/factory/BenzCarDriver.java
+++ src/com/pattern/lly/factory/BenzCarDriver.java
@@ -7,7 +7,7 @@ package com.pattern.lly.factory;
  * Time: 下午2:57
  */
 public class BenzCarDriver implements Driver {
-
+//this is a test
     @Override
     public Car driver() {
         return new BenzCar();
